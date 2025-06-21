e7197237b9feb7a25c1648b8ae6c2b6afcfb4100
berdancans <berdancans44@gmail.com>
2025-06-21 12:24:26 +0300

    light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
-import Head from "next/head"; // Bunu ekle
+import Head from "next/head";
 import "../styles/index.css";
 import { Providers } from "./providers";
 
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
 
-      <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+      <body className={`bg-[#FCFCFC] ${inter.className}`}>
         <Providers>
           <Header />
           {children}
